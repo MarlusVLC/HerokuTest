37fa4e43e7fca79adb2f4287f030c49816890c9c
MarlusVinicius <marlus.cabral@compasso.com.br>
2022-04-04 17:13:41 -0300

    basic server creation

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo :x');
+})
+
+app.listen(port, () => {
+    console.log('Server iniciado....');
+})
