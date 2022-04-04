343b1e7244ca31c246a9c0c36bcf0bb702e06f4b
MarlusVinicius <marlus.cabral@compasso.com.br>
2022-04-04 18:16:07 -0300

    create new route: player

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 
 var port = process.env.PORT || 80
 
+console.log("Diretório: " + __dirname);
+
 app.get('/', (req, res) => {
     res.send('Olá mundo :x');
+})
+
+app.get('/player', (req, res) => {
+    res.send('WLCOME TO HEEL sisisisi');
 })
 
 app.listen(port, () => {
